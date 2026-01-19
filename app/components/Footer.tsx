b9446e79204a57bc0a4aeb0e87595be2e623cc24
Abhay <abhay@cleardigital.com>
2026-01-19 16:39:21 +0530

    set layout

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <footer className='text-center mt-8'>
+       <p>&copy; 2026 Raddy &middot; Built with Next Js</p>
+    </footer>
+  )
+}
+
+export default Footer
